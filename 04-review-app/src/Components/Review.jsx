5de452c3ca84a01592c3620bd8ec2ecded200071
Nkosikenny <kennybhekumuzi@gmail.com>
2025-04-24 16:29:08 +0200

    Refactor Review component for improved code readability and consistency

--- 04-review-app/src/Components/Review.jsx
+++ 04-review-app/src/Components/Review.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-
   const [index, setIndex] = useState(0);
   const [person, setPerson] = useState(people[0]);
 
@@ -11,18 +10,38 @@ const Review = () => {
     const newIndex = (index + 1) % people.length;
     setIndex(newIndex);
     setPerson(people[newIndex]);
-  }
+  };
 
-  
+  const prevPerson = () => {
+    const newIndex = (index - 1 + people.length) % people.length;
+    setIndex(newIndex);
+    setPerson(people[newIndex]);
+  };
+
+  const randomPerson = () => {
+    const newIndex = Math.floor(Math.random() * people.length);
+    setIndex(newIndex);
+    setPerson(people[newIndex]);
+  };
 
   return (
     <article>
       <img src={person.image} alt={person.name} />
       <h4>{person.name}</h4>
       <h4>{person.job}</h4>
-      <button type='type' onClick={nextPerson}>next person</button>
+      <button type="type" onClick={nextPerson}>
+        next person
+      </button>
+      <br />
+      <button type="type" onClick={prevPerson}>
+        previous person
+      </button>
+      <br />
+      <button type="type" onClick={randomPerson}>
+        random person
+      </button>
     </article>
-  )
+  );
 };
 
 export default Review;
