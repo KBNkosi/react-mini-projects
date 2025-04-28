18f03e07674cf173893980062294738066baabc6
Nkosikenny <kennybhekumuzi@gmail.com>
2025-04-28 15:58:38 +0200

    feat: implement Question component to display questions and toggle answers

--- 05-accordion-app/src/components/Question.jsx
+++ 05-accordion-app/src/components/Question.jsx
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import questions from "./data"
+
+
 const Question = () => {
-  return <h2>question component</h2>;
+  const [viewInfo, setViewInfo] = useState({});
+
+ 
+  const handleButtonClick=(id)=>{
+   setViewInfo((prevState)=>({
+     ...prevState,[id]:!prevState[id]
+   }))
+  }
+
+  return (
+    <>
+     {questions.map((question)=>(
+       <article>
+       <header>
+         <h4>{question.title}</h4>
+         <button type="button" onClick={()=>handleButtonClick(question.id)} >
+          {viewInfo ? <AiOutlineMinus />: <AiOutlinePlus />}
+         </button>
+        {viewInfo[question.id] ? <p>{question.info}</p>: ""}
+       </header>
+     </article>
+     ))}
+    </>
+  );
 };
 
 export default Question;
