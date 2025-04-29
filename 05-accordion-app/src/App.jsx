51a7981011786e93d54b112f9d358dd05ce043f5
Nkosikenny <kennybhekumuzi@gmail.com>
2025-04-29 08:46:25 +0200

    feat: refactor App and Question components for improved functionality and structure

--- 05-accordion-app/src/App.jsx
+++ 05-accordion-app/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import Question from './components/Question'
+import data from "./components/data"
 
 
 function App() {
-  
-
+  const [questions,setQuestions]=useState(data)
+ 
   return (
     <>
-      <Question />
+    <main>
+      <div className='container'>
+        <h3>questions and asnwers about login</h3>
+      <section className='info'> 
+      {questions.map((question)=>(
+        <Question key={question.id} {...question} />
+      ))}
+     </section>
+      </div>
+     
+    </main>
+      
     </>
   )
 }
--- 05-accordion-app/src/components/Question.jsx
+++ 05-accordion-app/src/components/Question.jsx
@@ -1,33 +1,21 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-import questions from "./data"
 
-
-const Question = () => {
-  const [viewInfo, setViewInfo] = useState({});
-
- 
-  const handleButtonClick=(id)=>{
-   setViewInfo((prevState)=>({
-     ...prevState,[id]:!prevState[id]
-   }))
-  }
+const Question = ({ title, info }) => {
+  const [viewInfo, setViewInfo] = useState(false);
 
   return (
-    <>
-     {questions.map((question)=>(
-       <article>
-       <header>
-         <h4>{question.title}</h4>
-         <button type="button" onClick={()=>handleButtonClick(question.id)} >
-          {viewInfo ? <AiOutlineMinus />: <AiOutlinePlus />}
-         </button>
-        {viewInfo[question.id] ? <p>{question.info}</p>: ""}
-       </header>
-     </article>
-     ))}
-    </>
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setViewInfo(!viewInfo)}>
+          {viewInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+        {viewInfo ? <p>{info}</p> : ""}
+      </header>
+    </article>
   );
 };
+
 
 export default Question;
