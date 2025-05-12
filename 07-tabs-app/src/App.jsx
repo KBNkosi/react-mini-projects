0df09abcc9ff9b2973d3fa8eeb9890c1496ed5a4
Nkosikenny <kennybhekumuzi@gmail.com>
2025-05-12 13:54:36 +0200

    feat: update App component to fetch and display user data with error handling

--- 07-tabs-app/src/App.jsx
+++ 07-tabs-app/src/App.jsx
@@ -1,17 +1,72 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const url = 'https://course-api.com/react-tabs-project';
+const url =
+  "https://api.allorigins.win/raw?url=" +
+  encodeURIComponent("https://course-api.com/react-tabs-project");
 
 function App() {
- const [users, setUsers]=useState();
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
- useEffect(())
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          mode: "cors",
+        });
+        if (!response.ok) throw new Error(`HTTP error! status: ${resp.status}`);
+        const data = await response.json();
+        setUsers(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error fetching Data</h2>;
+
+  const handleUserChange = (id) => {
+    const singleUser = users[id];
+    setUser(singleUser);
+  };
 
   return (
     <>
       <h2>Tab application</h2>
+
+      <section>
+        {users.map((user) => (
+          <div key={user.id}>
+            <button className="btn" onClick={()=>handleUserChange(user.id)}>{user.company}</button>
+          </div>
+        ))}
+      </section>
+
+      <section>
+        {user.map((item) => {
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <h4>{item.company}</h4>
+            <h4>{item.dates}</h4>
+            <ul>
+              <li>{item.duties}</li>
+            </ul>
+          </div>;
+        })}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
